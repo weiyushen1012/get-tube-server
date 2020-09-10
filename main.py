99c6c960770e0d080d0a3a1135bcab15017f7460
Weiyu Shen <weiyushen1012@gmail.com>
2020-09-09 22:55:00 -0400

    delete temp video file after client downloaded for 20 seconds

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import youtube_dl
 import pathlib
 import uuid
+import time
+import os
+import logging
+
 from flask import Flask, send_from_directory, request
 from threading import Thread
-
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -20,15 +23,22 @@ def download_tube_to_local(url, video_temp_filename):
         ydl.download([url])
 
 
+def delete_local_video(video_temp_filename):
+    global video_temp_path
+    time.sleep(20)
+    logging.info(msg=f'deleting temp file {video_temp_filename}')
+    os.remove(f'{video_temp_path}/{video_temp_filename}')
+
+
 @app.route('/download_from_tube', methods=['POST'])
 def download_from_tube():
     data = request.get_json()
     url = data['url']
     video_temp_filename = f'{uuid.uuid1()}.mp4'
 
-    thread = Thread(target=download_tube_to_local, args=(url, video_temp_filename,))
-    thread.start()
-    thread.join()
+    download_thread = Thread(target=download_tube_to_local, args=(url, video_temp_filename,))
+    download_thread.start()
+    download_thread.join()
 
     return {'filename': video_temp_filename}
 
@@ -36,7 +46,10 @@ def download_from_tube():
 @app.route('/send_video_file/<video_temp_filename>', methods=['GET'])
 def send_video_file(video_temp_filename):
     global video_temp_path
-    print(video_temp_path, video_temp_filename)
+
+    delete_video_thread = Thread(target=delete_local_video, args=(video_temp_filename,))
+    delete_video_thread.start()
+
     return send_from_directory(video_temp_path, filename=video_temp_filename, as_attachment=True)
 
 
